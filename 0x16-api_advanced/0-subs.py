a777bea3845eed09fce20d823ab1ac32354a8ede
Faith Warima Ng'endo <warimapharium@gmail.com>
2024-08-13 16:04:21 +0300

    Update 0-subs.py correction 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,25 @@
-mport requests
+#!/usr/bin/python3
+"""
+Module to query the Reddit API and return the number of subscribers
+"""
+
+import requests
+
 
 def number_of_subscribers(subreddit):
-    # Define the base URL for the Reddit API
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If the subreddit is invalid, returns 0.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Set a custom User-Agent to avoid being rate-limited by Reddit
-    headers = {"User-Agent": "subreddit-subscriber-checker/0.1"}
+    headers = {"User-Agent": "reddit-subscriber-checker/0.1"}
     
     try:
-        # Send a GET request to the subreddit URL
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the response status is 200 (OK)
         if response.status_code == 200:
             data = response.json()
-            # Return the number of subscribers
-            return data['data']['subscribers']
+            return data['data'].get('subscribers', 0)
         else:
-            # Return 0 if the subreddit is invalid or response code is not 200
             return 0
     except requests.RequestException:
-        # Return 0 if there was an issue with the request
         return 0
-
