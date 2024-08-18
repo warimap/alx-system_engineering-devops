7fc2b059a246b934813a5c599606823a43dda23b
Faith Warima Ng'endo <warimapharium@gmail.com>
2024-08-18 12:41:19 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""
-Module that queries the Reddit API and prints the titles of the
-first 10 hot posts listed for a given subreddit.
-"""
-
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot
-    posts listed for a given subreddit. If the subreddit is invalid,
-    print None.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "reddit-top-ten/0.1"}
-    params = {"limit": 10}
-    
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        if response.status_code == 200:
-            posts = response.json().get('data', {}).get('children', [])
-            for post in posts:
-                print(post['data'].get('title'))
-        else:
-            print(None)
-    except requests.RequestException:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
